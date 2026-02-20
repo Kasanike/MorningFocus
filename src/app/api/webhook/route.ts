25243dc9caf48a36b3814b70fe8712b8beb3940d
Kasanike <jkasanicky@gmail.com>
2026-02-20 15:51:49 +0100

    Fix stripe

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { createAdminClient } from "@/utils/supabase/admin";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-11-20.acacia",
+  apiVersion: "2026-01-28.clover",
 });
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
